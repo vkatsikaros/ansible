799b686595c38efaeb9888c26461077513632507
Vangelis Katsikaros <vkatsikaros@gmail.com>
2021-04-23 20:14:56 +0300

    Initial skeleton import

---
+++ README.md
@@ -0,0 +1,11 @@
+# Ansible
+
+## Laptop
+
+```
+# bootstrap ansible
+bash bootstrap.sh
+
+# run ansible 
+ansible -i hosts tasks.yml --ask-become-pass
+```
---
+++ bootstrap.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+
+USER_HOME='/home/vag'
+VENV_HOME="${USER_HOME}"/.venv
+
+mkdir -p "${USER_HOME}"/.local/bin
+
+python3 -m virtualenv "${VENV_HOME}"/ansible
+source "${VENV_HOME}"/ansible/bin/activate
+pip3 install pip -U
+pip3 install ansible
+deactivate
+ln -s "${VENV_HOME}"/bin/ansible "${USER_HOME}"/.local/bin
