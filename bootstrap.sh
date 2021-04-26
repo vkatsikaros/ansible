cca8869f7998ef0c3da9e304246e20150a3a74cf
Vangelis Katsikaros <vkatsikaros@gmail.com>
2021-04-26 22:41:59 +0300

    Fix typos

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,4 +14,4 @@ source "${VENV_HOME}"/ansible/bin/activate
 pip3 install pip -U
 pip3 install ansible
 deactivate
-ln -s "${VENV_HOME}"/bin/ansible-playbook"${USER_HOME}"/.local/bin
+ln -s "${VENV_HOME}"/ansible/bin/ansible-playbook "${USER_HOME}"/.local/bin
