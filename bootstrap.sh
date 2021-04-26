048ccaae1b641cb3782c34b5ce4f3042c213ebec
Vangelis Katsikaros <vkatsikaros@gmail.com>
2021-04-26 22:41:34 +0300

    Correct python virtualenv

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ VENV_HOME="${USER_HOME}"/.venv
 
 mkdir -p "${USER_HOME}"/.local/bin
 
-python3 -m virtualenv "${VENV_HOME}"/ansible
+python3 -m venv "${VENV_HOME}"/ansible
 source "${VENV_HOME}"/ansible/bin/activate
 pip3 install pip -U
 pip3 install ansible
